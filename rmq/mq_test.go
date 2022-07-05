30fabc237a7b3d6c844072d2bce03b080310fc08
jpbirdy <jiangpeng285@qq.com>
2022-07-05 10:12:29 +0800

    modify

--- rmq/mq_test.go
+++ rmq/mq_test.go
@@ -8,12 +8,16 @@ import (
 	"time"
 )
 
-func TestConsumerMessage(t *testing.T) {
-	agiRMQ := New("amqp://admin:lyntime123456@rabbitmq.lyntime.com:5672/")
+const (
+	queueName = "test.queue"
+)
 
-	agiRMQ.RMQConsumeWithGoroutine(
-		"test.queue",
-		"test.queue",
+func TestConsumerMessage(t *testing.T) {
+	rmq := New("amqp://admin:lyntime123456@rabbitmq.lyntime.com:5672/")
+
+	rmq.RMQConsumeWithGoroutine(
+		queueName,
+		queueName,
 		false,
 		4, func(body []byte) (ack bool, err error) {
 			log.Println(string(body))
@@ -29,7 +33,7 @@ func TestConsumerMessage(t *testing.T) {
 
 func TestPublishMessage(t *testing.T) {
 
-	agiRMQ := New("amqp://admin:lyntime123456@rabbitmq.lyntime.com:5672/")
+	rmq := New("amqp://admin:lyntime123456@rabbitmq.lyntime.com:5672/")
 
 	for i := 0; i < 10; i++ {
 		testBody := struct {
@@ -39,7 +43,7 @@ func TestPublishMessage(t *testing.T) {
 			Index: int64(i),
 			Time:  time.Now().Unix(),
 		}
-		err := agiRMQ.Publish(context.Background(), "test.queue", testBody)
+		err := rmq.Publish(context.Background(), queueName, testBody)
 		// err := agiRMQ.PublishWithUid(123, "tesla-test-be.delayMsg.postKey", testBody)
 		fmt.Println(i, err)
 		time.Sleep(1 * time.Second)
